58da05903d9d877067f0b6001282a99ed5a95fc6
Chandu JR <chandujr@gmail.com>
2019-03-25 00:29:59 +0530

    Add player prefs clearing editor extension

---
+++ Assets/Editor/Utils/ClearData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ClearData
+{
+    [MenuItem("Tools/Clear Player Prefs")]
+    private static void ClearPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Player Prefs cleared");
+    }
+}
